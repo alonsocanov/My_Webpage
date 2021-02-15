6c8dd4957a3cd1978178dce2bbb6b57c3ef6bff1
alonsocanov <alonsocanov@hotmail.com>
2021-02-15 12:04:16 -0600

    Experiences full table for my profile

--- application/database/migrations/010_Experience.php
+++ application/database/migrations/010_Experience.php
@@ -6,47 +6,76 @@ class Migration_Experience extends CI_Migration {
 		$this->dbforge->add_field(array(
 			'id' => array(
 				'type' => 'INT',
-				'constraint' => 11,
+				'constraint' => '11',
 				'auto_increment' => TRUE
 			),
-			'company' => array(
+			'kind' => array(
+				'type' => 'INT',
+				'constraint' => '16',
+				'null' => TRUE
+			),
+			'title' => array(
 				'type' => 'VARCHAR',
-				'constraint' => 64
+				'constraint' => '64',
+				'null' => TRUE
+			),
+			'description' => array(
+				'type' => 'LONGTEXT',
+				'null' => TRUE
 			),
 			'position' => array(
-				'type' => 'VARCHAR',
-				'constraint' => 128,
-				'null' => TRUE
-			),
-
-			'priority' => array(
-				'type' => 'INTEGER',
-				'constraint' => 11,
-				'null' => TRUE
-			),
-			'logo' => array(
 				'type' => 'VARCHAR',
 				'constraint' => '128',
 				'null' => TRUE
 			),
-			'logo_url' => array(
+			'priority' => array(
+				'type' => 'INT',
+				'constraint' => '11',
+				'null' => TRUE
+			),
+			'image' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '128',
+				'null' => TRUE
+			),
+			'image_url' => array(
 				'type' => 'VARCHAR',
 				'constraint' => '254',
 				'null' => TRUE
 			),
+			'video' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '128',
+				'null' => TRUE
+			),
+			'video_url' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '254',
+				'null' => TRUE
+			),
+			'url' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '254',
+				'null' => TRUE
+			),
+			'faicon' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '32',
+				'null' => TRUE
+			),
 			'start_date' => array(
 				'type' => 'VARCHAR',
-				'constraint' => 32,
+				'constraint' => '32',
 				'null' => TRUE
 			),
 			'end_date' => array(
 				'type' => 'VARCHAR',
-				'constraint' => 32,
+				'constraint' => '32',
 				'null' => TRUE
 			),
 			'location' => array(
 				'type' => 'VARCHAR',
-				'constraint' => 32,
+				'constraint' => '64',
 				'null' => TRUE
 			),
 			'content' => array(
@@ -62,37 +91,33 @@ class Migration_Experience extends CI_Migration {
 			)
 		));
 
+
+
 		$this->dbforge->add_key('id', TRUE);
 		$this->dbforge->create_table('experience');
 
 		$data = [];
-		$data['company'] = 'Ixaya';
+		$data['title'] = 'Ixaya';
 		$data['position'] = 'Deep Learning and Web developer';
 		$data['priority'] = 1;
-		$data['logo'] = '';
-		$data['logo_url'] = '';
 		$data['start_date'] = 'May 2020';
 		$data['end_date'] = '';
 		$data['location'] = 'Leon, Gto';
 		$data['content'] = '<div class="page" title="Page 1"><div class="layoutArea"><div class="column"><ul><li><p>The objective is to extract key data from policy documents in order to generate a data base</p></li><li><p>Used Deep Learning and Image Processing techniques for document classification and data extraction</p></li><li><p>Facilitates workers’ data filling process into the system</p></li></ul></div></div></div>';
 		$this->db->insert('experience', $data);
 
-		$data['company'] = 'AFD Technologies';
+		$data['title'] = 'AFD Technologies';
 		$data['position'] = 'Machine Learning Project Manager';
 		$data['priority'] = 1;
-		$data['logo'] = '';
-		$data['logo_url'] = '';
 		$data['start_date'] = 'May 2020';
 		$data['end_date'] = '';
 		$data['location'] = 'Paris, France';
 		$data['content'] = '<div class="page" title="Page 1"><div class="layoutArea"><p></p><p></p><ul><ul></ul></ul><p></p><p></p><p></p><ul><ul></ul></ul><p></p><p></p><ul></ul><p></p><ul><li>The goal of the project was to detect alarm anomalies on&nbsp;telecommunication&nbsp;services provided by Orange France</li></ul><p></p><ul></ul><p></p><ul><li>Used unsupervised algorithms for anomaly detection of alarm systems</li></ul><p></p><ul></ul><p></p><ul><li>Evaluated unsupervised algorithms with statistical criteria in order to determine the best-one according to data set</li></ul><p></p><ul></ul><p></p><ul><li>The models were implemented for testing phase</li></ul><p></p><ul></ul><p></p><ul><li>The time that the supervision team took to detect certain anomalies was&nbsp;reduced</li></ul></div></div>';
 		$this->db->insert('experience', $data);
 
-		$data['company'] = 'Electronics and Electromagnetism Laboratory (L2E)';
+		$data['title'] = 'Electronics and Electromagnetism Laboratory (L2E)';
 		$data['position'] = 'Reserch Internship';
 		$data['priority'] = 1;
-		$data['logo'] = '';
-		$data['logo_url'] = '';
 		$data['start_date'] = 'May 2020';
 		$data['end_date'] = '';
 		$data['location'] = 'Paris, France';
