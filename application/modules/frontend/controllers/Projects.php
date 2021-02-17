c3007ab51c92736bf52df143b26e4b2615f800c8
alonsocanov <alonsocanov@hotmail.com>
2021-02-17 15:31:53 -0600

    Added Projects Page

--- application/modules/frontend/controllers/Projects.php
+++ application/modules/frontend/controllers/Projects.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Projects extends MY_Controller {
+class Projects extends Public_Controller {
 
     function __construct() {
 
